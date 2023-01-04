b0cbb7f9f38e1a2aa905f40f2dfc313d641eec6d
N Z <nelectrik@gmail.com>
2023-01-04 23:46:50 +1030

    Employee class and constructor code added

---
+++ _test_/Employee.test.js
@@ -0,0 +1,21 @@
+const Manager = require("./manager");
+const Engineer = require("./engineer");
+const Intern = require("./intern");
+
+class Employee {
+  constructor(name) {
+    this.name = name;
+    this.id = id;
+    this.email = email;
+  }
+}
+
+// name
+// id
+// email
+// getName()
+// getId()
+// getEmail()
+// getRole()—returns 'Employee'
+
+//Employee, Manager, Engineer, and Intern classes.
